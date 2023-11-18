710c91cadeb8d03132e9f5ed8cdd748ed7ad42d9
Divyatech-hub <divyalakshmi5804@gmail.com>
2023-11-18 18:33:26 -0500

    updated smart contract class

--- smart_contract/smart_contract.js
+++ smart_contract/smart_contract.js
@@ -1,35 +1,87 @@
+const { Client, AccountBalanceQuery, Hbar, AccountCreateTransaction, TransferTransaction } = require("@hashgraph/sdk");
+
 class HederaSmartContract {
     constructor() {
-        this.totalFunds = 0;
+        this.client = Client.forTestnet(); // Replace with appropriate network client setup
+        this.operatorKey = process.env.OPERATOR_KEY; // Replace with your operator key
+        this.operatorId = process.env.OPERATOR_ID; // Replace with your operator ID
+        this.contractAccount = null;
     }
 
-    // Method to receive funds from a donor
-    receiveFunds(amount) {
-        // Simulating receiving funds from a donor on the Hedera network
-        // Here you might implement logic to interact with Hedera's API to credit the contract with the received funds
-        this.totalFunds += amount;
-        console.log(`Received ${amount} Hedera coins. Total funds in the contract: ${this.totalFunds}`);
-    }
+    async setupContract() {
+        try {
+            const operatorId = this.operatorId;
+            const operatorKey = this.operatorKey;
 
-    // Method to transfer funds to an account on the Hedera network
-    transferFunds(amount, recipientAccount) {
-        // Simulating transferring funds to a recipient account on the Hedera network
-        // Here you might implement logic to interact with Hedera's API to transfer funds
-        if (amount > this.totalFunds) {
-            console.log(`Insufficient funds in the contract.`);
-            return;
+            this.client.setOperator(operatorId, operatorKey);
+
+            const receipt = await new AccountCreateTransaction()
+                .setKey(operatorKey)
+                .execute(this.client);
+
+            const newAccountId = receipt.getAccountId();
+            console.log(`Contract account created: ${newAccountId}`);
+
+            this.contractAccount = newAccountId;
+        } catch (error) {
+            console.error("Error setting up contract:", error);
         }
+    }
 
-        this.totalFunds -= amount;
-        console.log(`Transferred ${amount} Hedera coins to ${recipientAccount}. Remaining funds: ${this.totalFunds}`);
+    async getContractBalance() {
+        try {
+            const balance = await new AccountBalanceQuery()
+                .setAccountId(this.contractAccount)
+                .execute(this.client);
+
+            return balance.hbars.toTinybars(); // Convert to smallest unit (tinybars)
+        } catch (error) {
+            console.error("Error getting contract balance:", error);
+        }
+    }
+
+    async receiveFunds(amount) {
+        try {
+            const transferTransaction = await new TransferTransaction()
+                .addHbarTransfer(this.client.operatorAccountId, new Hbar(amount))
+                .addHbarTransfer(this.contractAccount, new Hbar(-amount))
+                .execute(this.client);
+
+            console.log(`Received ${amount} Hedera coins. Transaction ID: ${transferTransaction.transactionId}`);
+        } catch (error) {
+            console.error("Error receiving funds:", error);
+        }
+    }
+
+    async transferFunds(amount, recipientAccountId) {
+        try {
+            const transferTransaction = await new TransferTransaction()
+                .addHbarTransfer(this.contractAccount, new Hbar(-amount))
+                .addHbarTransfer(recipientAccountId, new Hbar(amount))
+                .execute(this.client);
+
+            console.log(`Transferred ${amount} Hedera coins to ${recipientAccountId}. Transaction ID: ${transferTransaction.transactionId}`);
+        } catch (error) {
+            console.error("Error transferring funds:", error);
+        }
     }
 }
 
 // Example usage
-const contract = new HederaSmartContract();
+(async () => {
+    const contract = new HederaSmartContract();
 
-// Simulating receiving funds from a donor
-contract.receiveFunds(100); // Receive 100 Hedera coins
+    // Setup contract account
+    await contract.setupContract();
 
-// Simulating transferring funds to a recipient account
-contract.transferFunds(50, 'RecipientAccount123'); // Transfer 50 Hedera coins
+    // Get contract balance
+    const contractBalance = await contract.getContractBalance();
+    console.log(`Contract balance: ${contractBalance} tinybars`);
+
+    // Simulating receiving funds
+    await contract.receiveFunds(100);
+
+    // Simulating transferring funds to a recipient account
+    const recipientAccountId = 'RECIPIENT_ACCOUNT_ID'; // Replace with recipient's account ID
+    await contract.transferFunds(50, recipientAccountId);
+})();
